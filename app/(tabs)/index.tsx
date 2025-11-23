ec410e90c69ceaf411c063836b9c74515779e2e5
Nurodayo <cianuroicecream@gmail.com>
2025-11-23 18:42:56 -0300

    Inicio de un esqueleto de event y su respectivo styling en la tab Home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
 import { View, Text, StyleSheet } from 'react-native';
+import EventCard from 'components/EventCard';
+import { useGetEvents } from '@/useHooks/useEvents';
+import { useEffect } from 'react';
 
 export default function Tab() {
+  const { getEvents, data, isLoading, error } = useGetEvents(); // hay que modificar el hook para el lazy scrolling
+
+  const events = data?.data || [];
+
+  useEffect(() => {
+    getEvents({ limit: 20 });
+  }, []);
+
+  console.log(events);
   return (
     <View style={styles.container}>
-      <Text>Tab [Home|Settings]</Text>
+      <EventCard />
     </View>
   );
 }
