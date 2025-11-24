07de55dcfc3afbdd6efec68ac02ad39796be55e2
Nurodayo <cianuroicecream@gmail.com>
2025-11-23 21:23:01 -0300

    Render de los eventCards en un View scrolleable TODO: arreglar que no se vean las fotos de la api de placeholders

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from 'react';
 import { View, Text, Image, Dimensions, StyleSheet } from 'react-native';
 
 const height = Dimensions.get('window').height;
-
+// no me funciono tailwind muy bien asi que lo hice asi
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
@@ -36,30 +37,64 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function EventCard() {
+export default function EventCard({ name, category, location, date, image }) {
+  //formatear titulo
+  // cambiamos la imagen a esto si no hay https://placehold.co/600x400?text=No+picture
+  const [picture, setPicture] = useState('');
+  const [title, setTitle] = useState('');
+
+  const fechaFormateada = new Date(date).toLocaleDateString('es-CL', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  useEffect(() => {
+    // Formateamos el titulo para que este bonito
+    const toTitle = (str) => {
+      return str
+        .toLowerCase()
+        .trim()
+        .split(/\s+/)
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+    };
+    console.log(image);
+    if (image.length < 1 || image === null) {
+      console.log('DEBUG no hay imagen');
+      setPicture('https://placehold.co/600x400?text=No+picture'); //creo que esta api me
+    } else {
+      setPicture(image);
+    }
+    setTitle(toTitle(name));
+  }, [name, image]);
+
   return (
     <View className="flex-row" style={styles.container}>
       <Image
         style={styles.eventPicture}
         source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
+          uri: picture,
         }}
       />
       <View style={styles.eventText}>
         <Text style={styles.eventTitle} numberOfLines={2} ellipsizeMode="tail">
-          Evento De Prueba Css amongus.
+          {title}
         </Text>
         <View className="category">
           <Text numberOfLines={1} ellipsizeMode="tail">
-            Categoria.
+            {category}
           </Text>
+          <Text>{fechaFormateada}</Text>
         </View>
         <View className="location" style={styles.eventLocation}>
           <Text numberOfLines={1} ellipsizeMode="tail">
-            Ubicacion.
+            {location}
           </Text>
         </View>
       </View>
     </View>
   );
 }
+
+// mas que nada faltaria agregar los iconos aca pero tu le sabes
