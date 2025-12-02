8ab429da44620150621f042176e57dea747a39d3
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2025-12-02 11:20:56 -0300

    fix: gitignore rules and feat: add SearchBar

---
+++ components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Text, View, Image, Dimensions, StyleSheet, Pressable, TextInput } from 'react-native'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
